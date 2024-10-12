b66bbb1cffe31ac2fbdbe4ecfc04d0ee859e2194
WallaceBarros <w.wallacebarros.cruz@gmail.com>
2024-10-12 14:46:57 -0300

    definindo as interações com a interface (js => html)

--- src/script.js
+++ src/script.js
@@ -1,10 +1,13 @@
-const nameP = document.querySelector("#pokemon-name")
-const img =  document.querySelector("#pokemon-img")
-const weight = document.querySelector("#pokemon-weight")
+const nameP = document.querySelector("#nameP")
+const img =  document.querySelector("#img")
+const type = document.querySelector("#type")
+const height = document.querySelector("#height")
+const ability = document.querySelector("#ability")
+const weight = document.querySelector("#weight")
 const btn = document.querySelector("button")
 
 nameP.textContent = " "
-img.src = "./pokebola.webp"
+img.src = "./pokebola.png"
 weight.textContent = " "
 
 btn.addEventListener('click', () => {
@@ -21,6 +24,14 @@ btn.addEventListener('click', () => {
         const ability = date.abilities[0].ability.name
         const weight = date.weight
         const height = date.height
+        
+        this.nameP.textContent = nameP
+        this.img.src = img
+        this.type.textContent = type
+        this.ability.textContent = ability
+        this.weight.textContent = weight
+        this.height.textContent = height
+
     })
     .catch(err => {
         nameP.textContent = "pokemon não encontrado"
